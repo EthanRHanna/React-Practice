8687a1e74ee366c482f9a23fad94f7a115394d4d
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-07-06 19:11:51 -0500

    Got the basic look
    Need to add something between the info block and the top of the page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import "../CSS/Home.css";
 import "../CSS/InfoBlocks.css";
 import "../CSS/ToolTip.css";
+import Hamburger from "../Images/RSHamburger.jpg";
 import { ButtonContent, Button, Icon, Container } from "semantic-ui-react";
 
 function InfoBlocks({ header, info, children }) {
@@ -21,9 +22,17 @@ function Home() {
         <div style={{ display: "flex" }}>
           <img
             src="https://static.wixstatic.com/media/41d000_af9107ca34395bbad48890262d578148.png/v1/fill/w_311,h_128,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/41d000_af9107ca34395bbad48890262d578148.png"
-            style={{ maxHeight: "50px" }}
+            class="IconImage"
           />
-          <h1>RiverSide Patty</h1>
+          <h1
+            style={{
+              marginLeft: "20px",
+              marginBottom: "15px",
+              fontSize: "45px",
+            }}
+          >
+            RiverSide Patty
+          </h1>
         </div>
         <div>
           <a href="https://www.facebook.com/p/Riverside-patty-100081724278918/">
@@ -33,7 +42,7 @@ function Home() {
       </header>
 
       <div class="TopImage">
-        <img src="../Images/RSHamburger.jpg" alt="Burgor" class="TopImage" />
+        <img src={Hamburger} class="TopImage" />
       </div>
 
       <div class="InfoSection">
