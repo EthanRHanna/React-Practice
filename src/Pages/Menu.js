c97e595a5491ccc4219292300717369bbbf968e8
EthanRHanna <42752797+EthanRHanna@users.noreply.github.com>
2024-07-08 19:26:01 -0500

    Started Menu
    Started the basic page. Navbar and a banner have been added.

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -1,5 +1,25 @@
+/* CSS Imports */
+import "../CSS/Menu.css";
+import "../CSS/Banner.css";
+
+/* Compnents Imports*/
+import NavBar from "../Components/NavBar";
+
+import Banner from "../Images/RSCounter.jpg";
+
 function Menu() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <div class="BannerDiv">
+        <img src="" class="BannerImg" />
+        <h1 class="BannerWords">Menu</h1>
+      </div>
+      <div class="SelectionMenu"></div>
+    </>
+  );
 }
 
 export default Menu;
